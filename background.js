8db29882a4f5da1c2dbd9d7d9af7505d344c3db0
alexandersideris <alexander.sideris.west@gmail.com>
2018-02-04 16:13:53 +0200

    Weird but first working version
    Works every two New Tab openings...

--- background.js
+++ background.js
@@ -1,24 +1,81 @@
 var extensions = [];
 var indexOfEnabledExtension = 0;
 
-chrome.tabs.onCreated.addListener(function() {
-	for(i=0; i<extensions.length; i++){
-		if(indexOfEnabledExtension == extensions.length-1){
-			chrome.management.setEnabled($extensions[$indexOfEnabledExtension], false, function() {});
-			chrome.management.setEnabled($extensions[0], true, function() {});
-			$indexOfEnabledExtension = 0;
-		}
-		chrome.management.setEnabled($extensions[$indexOfEnabledExtension], false, function() {});
-		chrome.management.setEnabled($extensions[$indexOfEnabledExtension+1], true, function() {});	
-		$indexOfEnabledExtension ++;
+var number = 0;
+
+function test_prime(n) {
+
+	if (n===1) {
+    	return false;
 	}
+  	else if(n === 2) {
+    	return true;
+  	}else{
+    	for(var x = 2; x < n; x++) {
+      		if(n % x === 0){
+        		return false;
+      		}
+    	}
+    	return true;  
+  	}
+
+}
+
+chrome.tabs.onCreated.addListener(function() {
+	console.log("From here babaaay -->")
+	console.log(extensions)
+
+	if(number == 0){
+		number = 1;
+		return
+	}
+
+
+	// Edge case
+	if(indexOfEnabledExtension == extensions.length-1){
+
+		console.log("Edge case")
+
+		chrome.management.setEnabled(extensions[indexOfEnabledExtension], false, function() {
+			console.log('disabled '+extensions[indexOfEnabledExtension])
+		});
+			
+		chrome.management.setEnabled(extensions[0], true, function() {
+			console.log('enabled '+ extensions[0])
+			indexOfEnabledExtension = 0;
+		});
+
+		
+		console.log('indexOfEnabledExtension is '+indexOfEnabledExtension)
+
+	// Normal case
+	}else{
+
+		console.log("Normal case")
+
+		chrome.management.setEnabled(extensions[indexOfEnabledExtension], false, function() {
+			console.log('disabled '+extensions[indexOfEnabledExtension])
+		});
+
+		chrome.management.setEnabled(extensions[indexOfEnabledExtension+1], true, function() {
+			console.log('enabled '+extensions[indexOfEnabledExtension+1])
+			indexOfEnabledExtension ++;
+		});
+
+		
+		console.log('indexOfEnabledExtension is '+indexOfEnabledExtension)
+
+	}
+
+	number = 0;
+		
 });
 
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	$extensions = [];
-  	$indexOfEnabledExtension = 0;
+  	extensions = [];
+  	indexOfEnabledExtension = 0;
 
     console.log(request.ids)
 
@@ -29,6 +86,13 @@ chrome.runtime.onMessage.addListener(
     for(i=0; i<request.ids.length; i++){
     	if(request.checked[i]){
     		extensions.push(request.ids[i])
+    	}
+    	
+    }
+
+    for(i=0; i<request.ids.length; i++){
+    	if(request.checked[i]){
+
     		chrome.management.setEnabled(request.ids[i], true, function() {
 	    		
 	   		});
@@ -37,6 +101,8 @@ chrome.runtime.onMessage.addListener(
     	}
     	
     }
+    console.log('extensions-->')
+    console.log(extensions)
 
     sendResponse({farewell: "goodbye"})
 
